7919902cea59867b15b43727059cfeda93a6f4c3
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-02-04 06:21:50 +0000

    update

--- namebase.js
+++ namebase.js
@@ -1,25 +1,29 @@
-const https = require('https');
+const got = require('got');
+const fs = require('fs');
 
 // const names = [];
+
+function sortByKey(array, key) {
+	return array.sort(function (a, b) {
+		var x = a[key]; var y = b[key];
+		return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+	});
+}
 
 (async () => {
 const dates = [];
 const dateToValue = {}; 
+const names = [];
+const nameToSale = {};
 
-for (let i = 0; i < 1000; i += 100) {
+const datesJSON = [];
+
+for (let i = 0; i < 20000; i += 100) {
   console.log(i);
-  https.get('https://www.namebase.io/api/domains/sold/' + i.toString(), (resp) => {
-    let data = '';
 
-    // A chunk of data has been received.
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
+  const response = await got('https://www.namebase.io/api/domains/sold/' + i.toString());
+  const finalData = JSON.parse(response.body);
 
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      const finalData = JSON.parse(data);
-      //console.log(finalData);
       for (let j = 0; j < finalData.domains.length; j++) {
         const sellDate = new Date(finalData.domains[j].created_at).toISOString().split('T')[0];
         if (!(sellDate in dateToValue)) {
@@ -27,14 +31,43 @@ for (let i = 0; i < 1000; i += 100) {
           dates.push(sellDate);
         }
         dateToValue[sellDate] += parseInt(finalData.domains[j].amount);
+        nameToSale[finalData.domains[j].name] = parseInt(finalData.domains[j].amount);
+        //names.push(finalData.domains[j].name);
         //console.log(dateToValue[sellDate], sellDate);
       }
-    });
 
-  }).on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
   
 }
-console.log(names);
-});
+
+for (const dateKey in dates) {
+  datesJSON.push({
+    "name": dates[dates.length - dateKey - 1],
+    "Sales": dateToValue[dates[dates.length - dateKey - 1]] / 1000000,
+  });
+}
+
+for (const property in nameToSale) {
+  names.push({
+    col1: property,
+    col2: nameToSale[property]
+  });
+}
+sortByKey(names, "col2");
+
+const returnFinalNames = [];
+	let counter = 1;
+	for (const property in names) {
+		returnFinalNames.push({
+			col1: counter,
+			col2: names[property].col1,
+			col3: names[property].col2 / 1000000,
+		});
+		counter++;
+	}
+
+console.log(returnFinalNames);
+
+fs.writeFileSync('namebase.json', JSON.stringify(returnFinalNames));
+fs.writeFileSync('namebasedates.json', JSON.stringify(datesJSON));
+
+})();
