5c81af3292baac715408649f6a55c65ca46c18a4
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-02-15 00:52:28 +0000

    fix

--- handshakeminutes.js
+++ handshakeminutes.js
@@ -42,7 +42,7 @@ const client = new NodeClient(clientOptions);
 	const dateBurns = {};
 	const dateNames = {};
 
-	for (let i = 53161; i < 53305; i++) {
+	for (let i = 54839; i < 54983; i++) {
 		console.log(i);
 		const contents = await client.execute('getblockbyheight', [i, 1, 1]);
 		const blockDate = new Date(contents.time * 1000).toISOString().substring(0, 16);
