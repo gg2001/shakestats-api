ca084208eb7da46ed8a4e06f65056e9331658583
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-03-03 12:16:37 +0000

    up

--- handshaketest.js
+++ handshaketest.js
@@ -52,7 +52,7 @@ const client = new NodeClient(clientOptions);
 	const blockBurns = {};
 	const blockNames = {};
 
-	for (let i = 56444; i < (clientinfo.chain.height); i++) {
+	for (let i = 57201; i < (clientinfo.chain.height); i++) {
 		console.log(i);
 		const contents = await client.execute('getblockbyheight', [i, 1, 1]);
 		const blockDate = new Date(contents.time * 1000).toISOString().split('T')[0];
